829693b4b0f57f514687826b2252e2985b137a20
Vince <45806875+vince1444@users.noreply.github.com>
2022-10-30 20:15:56 -0400

    Added genres logic and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './stylesheets/css/main.css';
+import Genres from './components/Genres';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="main">
+      <aside className="genres">
+        <Genres genres={ ['Ambient', 'Dungeon', 'Isolationism', 'Dreampunk', 'Breakcore', 'Future bass', 'UK bass', 'Wave', 'Acid breaks', 'Electro-disco', 'Eurodisco', 'Eurobeat', 'Spacesynth', 'Post-disco', 'Drumstep', 'Hardstep'] } />
+      </aside>
     </div>
   );
 }
