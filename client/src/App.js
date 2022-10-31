62cf7a1ff558d59be5ac2dc1ecfa7ee060abf0cf
Vince <45806875+vince1444@users.noreply.github.com>
2022-10-31 00:15:02 -0400

    Added lobbies css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './stylesheets/css/main.css';
 import Genres from './components/Genres';
+import Lobbies from './components/Lobbies';
 
 function App() {
   return (
     <div className="main">
-      <aside className="genres">
+      <header className="navigation">
+        <h2>Listen Party</h2>
+      </header>
+      <aside className="pane__left">
         <Genres genres={ ['Ambient', 'Dungeon', 'Isolationism', 'Dreampunk', 'Breakcore', 'Future bass', 'UK bass', 'Wave', 'Acid breaks', 'Electro-disco', 'Eurodisco', 'Eurobeat', 'Spacesynth', 'Post-disco', 'Drumstep', 'Hardstep'] } />
       </aside>
+      <main className="pane__main">
+        <Lobbies lobbies={ [{ name: 'Lobby 1', people: 5 }, { name: 'Lobby 2', people: 3 }, { name: 'Lobby 3' }] } />
+      </main>
     </div>
   );
 }
